002f0c4d9a90f9376d1bc26f1be5fd2761faf25a
Naveen Sundar Govindarajulu <naveensundarg@gmail.com>
2020-04-11 20:53:10 -0700

    Simpler Python example and interface.

--- interface.py
+++ interface.py
@@ -1,31 +1,24 @@
-import subprocess
 from py4j.java_gateway import JavaGateway
 
 gateway = None
 
 def start():
-    #global q,
     global gateway
-    if(not(gateway)):
-        #q = subprocess.Popen("mvn exec:java -Dexec.mainClass=com.naveensundarg.shadow.prover.Py4JServer".split())
+    if not (gateway):
         gateway = JavaGateway()
 
 def stop():
-    #global q,
     global gateway
-    if(gateway):
-        #q.kill()
-        gateway = None
+    gateway = None
 
 
 def prove(assumptions, goal):
 
     global gateway
-    if(not(gateway)):
+    if not gateway:
         start()
 
     lst = gateway.newEmptyList()
-
     for assumption in assumptions:
         lst.append(assumption)
 
