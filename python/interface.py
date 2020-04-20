7c13a3c691eca4e7b0402af82b1fc0e605d02d5c
Naveen Sundar Govindarajulu <naveensundarg@gmail.com>
2020-04-19 23:12:37 -0700

    New image for Python

---
+++ python/interface.py
@@ -0,0 +1,26 @@
+from py4j.java_gateway import JavaGateway
+from py4j.java_gateway import GatewayParameters
+
+gateway = None
+
+def start():
+    global gateway
+    if not (gateway):
+        gateway = JavaGateway(gateway_parameters=GatewayParameters(address=u'py-shadowprover'))
+
+def stop():
+    global gateway
+    gateway = None
+
+
+def prove(assumptions, goal):
+
+    global gateway
+    if not gateway:
+        start()
+
+    lst = gateway.newEmptyList()
+    for assumption in assumptions:
+        lst.append(assumption)
+
+    return gateway.prove(lst, goal)
